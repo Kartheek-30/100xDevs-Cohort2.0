5558849c68afedac5bb7b73b0d3729362e1d5dc2
Kartheek-30 <rajakarteek489@gmail.com>
2024-10-04 01:02:00 +0530

    Added solution for Week-2 File server problem

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,40 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+const filesDir = path.join(__dirname, 'files');
+
+app.get("/files", (req, res) => {
+  
+  fs.readdir(filesDir, (err, files) => {
+    if(err){
+      return res.status(500).json({ error: "Unable to read directory" });
+    } else {
+      res.status(200).json(files);
+    }
+  })
+})
+
+app.get("/file/:filename", (req, res) => {
+
+  const name = req.params.filename;
+  const filePath = path.join(filesDir, name);
+
+  if(fs.existsSync(filePath)){
+      fs.readFile(filePath, "utf-8", (err, data) => {
+          if(err){
+              res.status(500).json("Error reading file")
+          } else{
+              res.status(200).send(data);
+          }
+      });
+  } else{
+      res.status(404).send("File not found")
+  }
+});
+
+app.use((req, res) => {
+  res.status(404).send("Route not found")
+})
+
 
 module.exports = app;
