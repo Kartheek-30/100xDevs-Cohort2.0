b4cfb79d84be15b25e1b86e09f58639b89fcc9ba
Kartheek-30 <rajakarteek489@gmail.com>
2024-10-13 18:59:37 +0530

    Middlewares practice

--- Node/practice/middlewares.js
+++ Node/practice/middlewares.js
@@ -1,26 +1,62 @@
 const express = require("express");
 const app = express();
 
-app.get("/health-check", (req, res) => {
+function userMiddleware(req, res, next) {
     const username = req.headers.username;
     const password = req.headers.password;
+
+    if(username !== 'Kartheek' || password !== 'pass') {
+        res.status(403).json({
+            "msg" : "Invalid username"
+        });
+    } else {
+        next();
+    }
+};
+
+function kidneyMiddleware(req, res, next) {
     const kidneyId = +req.query.kidneyId;
 
-    if(username !== 'kartheek' || password !== 'pass'){
-        res.status(403).json({
-            "msg": "User data not found!"
-        });
-        return;
-    }
-
     if(kidneyId !== 1 && kidneyId !== 2) {
-        res.status(411).json({
-            "msg" : "Wrong Kidney ID!!!"
+        res.status(403).json({
+            "msg" : "Invalid kidney details"
         });
-        return;
+    } else {
+        next();
     }
+};
 
-    res.send("Your Kidney is fine!");
+app.get("/health-check", userMiddleware, kidneyMiddleware, (req, res) => {
+    res.send("Your health is fine");
+    });
+
+app.get("/kidney-check", kidneyMiddleware, (req, res) => {
+    res.send("Your kidney is Fine!");
 });
+
+
+
+// // Ugly way of declaring Methods
+// app.get("/health-check", (req, res) => {
+//     const username = req.headers.username;
+//     const password = req.headers.password;
+//     const kidneyId = +req.query.kidneyId;
+
+//     if(username !== 'kartheek' || password !== 'pass'){
+//         res.status(403).json({
+//             "msg": "User data not found!"
+//         });
+//         return;
+//     }
+
+//     if(kidneyId !== 1 && kidneyId !== 2) {
+//         res.status(411).json({
+//             "msg" : "Wrong Kidney ID!!!"
+//         });
+//         return;
+//     }
+
+//     res.send("Your Kidney is fine!");
+// });
 
 app.listen(3000);
