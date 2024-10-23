c8aa4ee0774618ccb595a78a9225fae67028429e
Kartheek-30 <rajakarteek489@gmail.com>
2024-10-23 23:04:08 +0530

    Added solutions for Week 2 Medium and Hard assignments

--- week-2/01-async-js/medium/clock.js
+++ week-2/01-async-js/medium/clock.js
@@ -4,7 +4,7 @@ function currentTime() {
     hours = String(hours).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
-    const ampm = date.getTime() >= 12 ? 'PM' : 'AM';
+    const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
 
 
     console.log(`${hours}:${minutes}:${seconds} ${ampm}`);
